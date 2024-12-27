194c772abfcd8bfb77ae6a34b34f8bd3e3bc3f8c
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-12-27 17:42:30 +0300

    program.cs configured

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,6 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
 //add container
 var app = builder.Build();
 //configure request pipeline
 
+app.MapCarter();
 app.Run();
