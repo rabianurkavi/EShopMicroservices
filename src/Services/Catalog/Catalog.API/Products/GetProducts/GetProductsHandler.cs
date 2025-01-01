073bf1b4f70975ad9c2be17893fe06b1e7542c2d
Rabia Nur Kavi <kavirabiaa@gmail.com>
2025-01-01 17:44:47 +0300

    handler created for getprodutct

---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,16 @@
+﻿namespace Catalog.API.Products.GetProducts
+{
+    public record GetProductsQuery() : IQuery<GetProductsResult>;
+    public record GetProductsResult(IEnumerable<Product> Products);
+    internal class GetProductsHandler(IDocumentSession documentSession, ILogger<GetProductsHandler> logger) 
+        : IQueryHandler<GetProductsQuery, GetProductsResult>
+    {
+        public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductQueryHandler.Handle called with {@Query}", query);
+            var products=await documentSession.Query<Product>().ToListAsync(cancellationToken);
+            return new GetProductsResult(products);
+
+        }
+    }
+}
