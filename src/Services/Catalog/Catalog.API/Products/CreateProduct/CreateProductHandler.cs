026e819ff4df1fbc0fce0913125a963d0e12b675
Rabia Nur Kavi <kavirabiaa@gmail.com>
2025-01-06 22:50:19 +0300

    validatorpipeline, fluentvalidation added

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,13 +1,28 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿using FluentValidation;
+
+namespace Catalog.API.Products.CreateProduct
 {
     public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-    internal class CreateProductCommandHandler(IDocumentSession documentSession) : ICommandHandler<CreateProductCommand, CreateProductResult>
+    public class CreateProductCommandValidator: AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductCommandValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required.");
+            RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required.");
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Description is required.");
+            RuleFor(x => x.ImageFile).NotEmpty().WithMessage("Image is required.");
+            RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0");
+            
+        }
+    }
+    internal class CreateProductCommandHandler(IDocumentSession documentSession, ILogger<CreateProductCommandHandler> logger) 
+        : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
-            //daha sonra mapster uygulayacağım
-
+            logger.LogInformation("CreateProductCommandHandler.Handle called with {@Command}", command);
+            
             var product = new Product
             {
                 Name = command.Name,
