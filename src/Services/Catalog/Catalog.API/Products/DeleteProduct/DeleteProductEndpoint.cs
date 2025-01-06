9d2e6b277bb1c24346fb974c9903173580f490ce
Rabia Nur Kavi <kavirabiaa@gmail.com>
2025-01-06 20:51:24 +0300

    DeleteProductResponse,DeleteProductEndpoint created

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -0,0 +1,26 @@
+﻿
+using Catalog.API.Products.UpdateProduct;
+
+namespace Catalog.API.Products.DeleteProduct
+{
+    public record DeleteProductResponse(bool IsSuccess);
+    public class DeleteProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/products/{id}",async (Guid id,ISender sender) =>
+            {
+                var result= await sender.Send(new DeleteProductCommand(id));
+                var response= result.Adapt<DeleteProductResponse>();
+                return Results.Ok(response);
+            })
+                .WithName("DeleteProduct")
+                .Produces<DeleteProductResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .Produces(StatusCodes.Status404NotFound)
+                .WithSummary("Delete Product")
+                .WithDescription("v Product");
+
+        }
+    }
+}
