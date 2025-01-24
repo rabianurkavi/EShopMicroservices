15dcec838d040f6c8f44b255e7a9439512bd756f
Rabia Nur Kavi <kavirabiaa@gmail.com>
2025-01-24 11:36:12 +0300

    new using name added

--- src/Services/Catalog/Catalog.API/GlobalUsing.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -8,6 +8,7 @@ global using Catalog.API.Exceptions;
 global using FluentValidation;
 global using BuildingBlocks.Behaviors;
 global using BuildingBlocks.Exceptions.Handler;
+global using Catalog.API.Data;
 
 
 
